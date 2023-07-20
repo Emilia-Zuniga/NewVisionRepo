25299ded854a6bec31b80e9cc334ba6e97d8fa09
Emilia Zuñiga Losada <emiliazlosada14@outlook.com>
2023-07-19 18:07:07 -0600

    Initial Commit

---
+++ NewVision/NewVisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewVisionApp.swift
+//  NewVision
+//
+//  Created by Emilia Zuñiga Losada on 19/07/23.
+//
+
+import SwiftUI
+
+@main
+struct NewVisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
